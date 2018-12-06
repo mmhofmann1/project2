d2c942723b67c0f932f44d1a8a998b9fe64d7b81
Matthew Hofmann <mhofmann@hawk.iit.edu>
2018-12-05 21:44:16 -0600

    Add comments

--- site.js
+++ site.js
@@ -1,5 +1,8 @@
+//listener for toggling menu navigation
 document.getElementById('menub').addEventListener("click", function() {
     if (links.style.display === "none") {
       links.style.display = "block"}
       else links.style.display = "none";
 });
+
+//listener for toggling dog gallery
