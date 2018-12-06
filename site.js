9b1dc444736e7e5133d7a0e7267e6db4dd77274d
Matthew Hofmann <mhofmann@hawk.iit.edu>
2018-12-05 18:13:31 -0600

    Add toggle menu button

---
+++ site.js
@@ -0,0 +1,5 @@
+document.getElementById('menub').addEventListener("click", function() {
+    if (links.style.display === "none") {
+      links.style.display = "block"}
+      else links.style.display = "none";
+});
